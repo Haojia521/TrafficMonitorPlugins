769e54a8f12c5d6ee214b448466dd53befa01b65
haojia <haojia521@hotmail.com>
2022-06-30 11:30:12 +0800

    add logo images which represent different states

--- PomodoroTimer/Resource.h
+++ PomodoroTimer/Resource.h
@@ -1,9 +1,11 @@
-﻿//{{NO_DEPENDENCIES}}
+//{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ 生成的包含文件。
 // 供 PomodoroTimer.rc 使用
 //
 #define IDI_ICON_LOGO                   1000
 #define IDR_MENU_MAIN                   1001
+#define IDI_ICON_LOGO_STOP              1002
+#define IDI_ICON_LOGO_BREAK             1003
 #define ID_FUNC_32771                   32771
 #define ID_FUNC_32772                   32772
 #define ID_FUNC_32773                   32773
@@ -19,7 +21,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        1002
+#define _APS_NEXT_RESOURCE_VALUE        1004
 #define _APS_NEXT_COMMAND_VALUE         32782
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           1000
