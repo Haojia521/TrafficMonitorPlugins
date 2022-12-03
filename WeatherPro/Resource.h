6f6828ef65652415dbf44cee0969e129cac8ce06
haojia <haojia521@hotmail.com>
2022-12-03 18:09:36 +0800

    update dialogs for English

--- WeatherPro/Resource.h
+++ WeatherPro/Resource.h
@@ -247,7 +247,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        1200
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1031
+#define _APS_NEXT_CONTROL_VALUE         1033
 #define _APS_NEXT_SYMED_VALUE           1000
 #endif
 #endif
